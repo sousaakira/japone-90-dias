67aa834e023a17a0be5e6d2bdb0d67f18ebb832c
Akira Sousa <sousa.akira@gmail.com>
2022-06-06 01:30:55 -0300

    fix: :bug: corrigindo diretorio

--- gulp/media/gulpfile.js
+++ gulp/media/gulpfile.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 function copy(){
   return src('../../src/media/**').pipe(
-    dest(path.resolve(__dirname, '../../aulas/pages/Inicio/media'))
+    dest(path.resolve(__dirname, '../../docs/pages/Inicio/media'))
   );
 }
 exports.default = copy;
